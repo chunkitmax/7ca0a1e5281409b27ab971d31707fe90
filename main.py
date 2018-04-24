c4f329eaa3b575d39a48746a6e58e8316d995574
chunkitmax <chunkitmax1125@gmail.com>
2018-04-24 12:22:01 +0800

    Allow to set verbose level

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ parser.add_argument('-emb', '--emb_len', default=100, type=int,
                     help='Embedding length')
 parser.add_argument('-g', '--gpu', action='store_true', help='GPU support')
 parser.add_argument('-tb', '--tensorboard', action='store_true', help='TensorBoard')
+parser.add_argument('-v', '--verbose', default=1, type=int, help='Verbose level')
 
 Args = parser.parse_args()
 
@@ -22,7 +23,7 @@ def main():
     return
   trainer = Trainer(is_many_to_one, embedding_len=Args.emb_len, max_epoch=Args.epoch,
                     batch_size=Args.batch_size, use_tensorboard=Args.tensorboard,
-                    use_cuda=Args.gpu, save_best_model=True)
+                    use_cuda=Args.gpu, save_best_model=True, verbose=Args.verbose)
   trainer.train()
 
 if __name__ == '__main__':
